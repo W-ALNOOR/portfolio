feb2a39c0a3e862b6ccf42e0867d62ed556f8202
W-ALNOOR <walnoor@my.centennialcollege.ca>
2025-09-27 03:58:06 -0400

    Update App.jsx
    update App page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MainRouter from "./MainRouter";
 
+// here the Main App component wraps the site in react Router
+
 const App = () => {
   return (
     <Router>
